97f7f7517858961e86f9fa9717cec937f316b131
shailesh gore <shaileshgore1998@gmail.com>
2025-09-23 10:58:08 +0530

    edit: edited in the app and counter

--- jest-test/src/Counter.jsx
+++ jest-test/src/Counter.jsx
@@ -4,12 +4,14 @@ import React, { useState } from "react";
 export default function Counter() {
   const [count, setCount] = useState(0);
 
+
   return (
     <div>
       <h1 data-testid="count-value">{count}</h1>
       {/* data managing */}
       <button className="inc" id="inc-count" autoFocus="true" onClick={() => setCount(count + 1)}>Increment</button>
-      <button className="dec" id="dec-count" autoFocus="true"  onClick={() => setCount(count - 1)}>Decrement</button>
+      <button className="dec" id="dec-count" autoFocus="true" onClick={() => setCount(count - 1)}>Decrement</button>
+      <h1>I have added Something</h1>
     </div>
   );
 }
