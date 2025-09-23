e29041d0ebbc866f47fe43ca5c98f4c3a3d7598f
shailesh gore <shaileshgore1998@gmail.com>
2025-09-23 11:04:14 +0530

    added the form component

--- jest-test/src/App.jsx
+++ jest-test/src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Counter from './Counter'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Counter from "./Counter";
+import Form from "./Form";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-   <Counter/>
-  )
+    <>
+      {" "}
+      <Counter />
+      <Form />
+    </>
+  );
 }
 
-export default App
+export default App;
